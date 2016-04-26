f986589a104353a5ab0845bc7eb58a498e6cd3b7
keiichi kobayashi <k.kobayashi@logic-design.jp>
2016-04-26 20:45:33 +0900

    [add]設定用の値を定数として登録

--- src/js/lib/const.js
+++ src/js/lib/const.js
@@ -1,3 +1,5 @@
+import _            from 'lodash';
+
 export default {
   // ***************************
   //  ポモドーロ時間(ms)
@@ -11,5 +13,12 @@ export default {
 
   // LONG_BREAK_AFTER:4,
   LONG_BREAK_AFTER:2,
+
+  OPTIONS_RANGE:{
+    POMODORO_DURATION:  _.range(5, 61, 5),
+    SHORT_BREAK:        _.range(1, 11, 1),
+    LONG_BREAK:         _.range(2, 21, 1),
+    LONG_BREAK_AFTER:   _.range(1, 11, 1),
+  }
 
 };
