a4093413f5e3085199a47635107974eb5627588f
keiichi kobayashi <k.kobayashi@logic-design.jp>
2016-04-26 22:26:09 +0900

    [modify]定数の変更。一般的なポモドーロ時間

--- src/js/lib/const.js
+++ src/js/lib/const.js
@@ -4,12 +4,11 @@ export default {
   // ***************************
   //  ポモドーロ時間(ms)
   POMODORO_DURATION: 25 * 60 * 1000,
-  // POMODORO_DURATION: 0.1 * 60 * 1000,
 
-  // SHORT_BREAK: 5 * 60 * 1000,
-  SHORT_BREAK: 2 * 1000,
-  // LONG_BREAK: 15 * 60 * 1000,
-  LONG_BREAK: 4 * 1000,
+  SHORT_BREAK: 5 * 60 * 1000,
+
+  LONG_BREAK: 15 * 60 * 1000,
+
 
   // LONG_BREAK_AFTER:4,
   LONG_BREAK_AFTER:2,
@@ -17,7 +16,7 @@ export default {
   OPTIONS_RANGE:{
     POMODORO_DURATION:  _.range(5, 61, 5),
     SHORT_BREAK:        _.range(1, 11, 1),
-    LONG_BREAK:         _.range(2, 21, 1),
+    LONG_BREAK:         _.range(5, 31, 1),
     LONG_BREAK_AFTER:   _.range(1, 11, 1),
   }
 
